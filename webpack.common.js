c829847c4b39b6daf11402af55f08f95de68703d
SoTilted <103329657+SoTilted@users.noreply.github.com>
2024-06-29 13:22:58 +0300

    Tried to fix wav audio to find out I probably need to change the bitrate so i converted the files to wav

--- webpack.common.js
+++ webpack.common.js
@@ -1,54 +1,53 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-
   entry: {
-    app: './src/index.js',
+    app: "./src/index.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
-        type: 'asset/resource',
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(mp3|wav)$/i,
+        type:"asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        }
-      }
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
     ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template:'src/index.html',
-      title: 'AcousticTest',
-      filename: 'index.html',
-      inject: 'body',
+      template: "src/index.html",
+      title: "AcousticTest",
+      filename: "index.html",
+      inject: "body",
     }),
-
   ],
 
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-
 };
